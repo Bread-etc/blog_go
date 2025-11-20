0fc7296b6522f27284e4190248cf309fb6cbe28b
bread_etc <1054115624@qq.com>
2025-11-20 23:30:12 +0800

    feat(pkg): 新增返回统一响应格式的响应体工具包

---
+++ pkg/response/response.go
@@ -0,0 +1,41 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Response 基础响应结构体
+type Response struct {
+	Code    int         `json:"code"`    // 业务状态码
+	Message string      `json:"message"` // 提示信息
+	Data    interface{} `json:"data"`    // 数据
+}
+
+// Success 成功响应
+func Success(c *gin.Context, data interface{}) {
+	c.JSON(http.StatusOK, Response{
+		Code:    200,
+		Message: "success",
+		Data:    data,
+	})
+}
+
+// Error 错误响应
+func Error(c *gin.Context, httpCode int, msg string) {
+	c.JSON(httpCode, Response{
+		Code:    httpCode,
+		Message: msg,
+		Data:    nil,
+	})
+}
+
+// ErrorWithCode 自定义业务码的错误响应
+func ErrorWithCode(c *gin.Context, httpCode int, businessCode int, msg string) {
+	c.JSON(httpCode, Response{
+		Code:    businessCode,
+		Message: msg,
+		Data:    nil,
+	})
+}
